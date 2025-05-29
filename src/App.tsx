5dc1e4aee68305ae1bd735088aa6c0e2c5b91fcc
Mesmarize <35055222+anilch1993@users.noreply.github.com>
2025-05-29 20:13:47 +0530

    Added the comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Header, SideBar } from "./_lib/components";
 import { AppContext } from "./_lib/context/app.context";
 import { StudentsRoutesList } from "./_lib/routes-json/student";
 import "./App.scss";
-
+/**
+ * Main Comonent
+ */
 function App() {
   const [toggle, setToggle] = useState(false);
 
@@ -12,6 +14,7 @@ function App() {
   }
   return (
     <>
+    
       <AppContext.Provider value={{toggle, toggleSideBar}}>
         <Header />
         <SideBar routesData={StudentsRoutesList} />
